eebb19a9b72bacf4d20a9e42653e563ab4e9afd2
German Arenas <german.arenass@autonoma.edu.co>
2024-08-23 18:29:25 -0500

    test login

--- cypress/e2e/saucedemo/login.cy.js
+++ cypress/e2e/saucedemo/login.cy.js
@@ -1,33 +1,37 @@
 describe('Login Tests', () => {
   const users = [
-    { username: 'standard_user', password: 'secret_sauce' },
     { username: 'locked_out_user', password: 'secret_sauce' },
+    { username: 'standard_user', password: 'secret_sauce' },
     { username: 'problem_user', password: 'secret_sauce' },
     { username: 'performance_glitch_user', password: 'secret_sauce' }
   ];
 
-  // Se ejecuta antes de cada prueba
+
   beforeEach(() => {
     cy.visit('https://www.saucedemo.com/v1/');
   });
 
   users.forEach(user => {
-    it(`Debe poder iniciar sesión con ${user.username}`, () => {
-      cy.get('#user-name').type(user.username);
-      cy.get('#password').type(user.password);
-
-      cy.contains('LOGIN').click();
-
-      if (user.username === 'locked_out_user') {
+    if (user.username === 'locked_out_user') {
+      it(`Debe bloquear el login para el usuario ${user.username}`, () => {
+        cy.get('#user-name').type(user.username);
+        cy.get('#password').type(user.password);
+  
+        cy.contains('LOGIN').click();
         cy.get('[data-test="error"]').should('contain', 'Sorry, this user has been locked out.');
-      } else {
+      });
+    } else {
+      it(`Debe poder hacer login con usuario ${user.username}`, () => {
+        cy.get('#user-name').type(user.username);
+        cy.get('#password').type(user.password);
+  
+        cy.contains('LOGIN').click();
+  
         cy.url().should('include', '/inventory.html');
-      }
-
-      if (user.username !== 'locked_out_user') {
         cy.get('.bm-burger-button').click();
         cy.get('#logout_sidebar_link').click();
-      }
-    });
+      });
+    }
   });
+  
 });
