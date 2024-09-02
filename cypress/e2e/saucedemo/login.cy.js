a07cb425d44aeeafa2d86627fe8c7e4005ba9298
German Arenas <german.arenass@autonoma.edu.co>
2024-09-01 20:20:07 -0500

    add command for login and fixture with user data

--- cypress/e2e/saucedemo/login.cy.js
+++ cypress/e2e/saucedemo/login.cy.js
@@ -1,43 +1,45 @@
 describe('Login Tests', () => {
-  const users = [
+  /* const users = [
     { username: 'locked_out_user', password: 'secret_sauce' },
     { username: 'standard_user', password: 'secret_sauce' },
     { username: 'problem_user', password: 'secret_sauce' },
     { username: 'performance_glitch_user', password: 'secret_sauce' }
-  ];
+  ]; */
+
+ let dataUsers; 
+
+  before (() => {
+    cy.fixture("users.json").then((data) => {
+      cy.log(data);
+      dataUsers = data;
+    })
+  });
 
   beforeEach(() => {
     cy.visit('/');
   });
 
-  users.forEach(user => {
-    if (user.username === 'locked_out_user') {
-      it(`Debe bloquear el login para el usuario ${user.username}`, () => {
-        cy.get('#user-name').type(user.username);
-        cy.get('#password').type(user.password);
-  
-        cy.contains('LOGIN').click();
-        cy.get('[data-test="error"]').should('contain', 'Sorry, this user has been locked out.');
-      });
-    } else {
-      it(`Debe poder hacer login con usuario ${user.username}`, () => {
-        cy.get('#user-name').type(user.username);
-        cy.get('#password').type(user.password);
-  
-        cy.contains('LOGIN').click();
-  
-        cy.url().should('include', '/inventory.html');
-        cy.go('back');
-      });
-    }
+  it('Login', function() {
+    dataUsers.users.forEach((user) => {
+      if (user.username === 'locked_out_user') {
+        cy.log(`Debe bloquear el login para el usuario ${user.username}`);
+        cy.login(user.username, user.password);
+        cy.get('[data-test="error"]')
+          .should("contain","Sorry, this user has been locked out.");
+      } else {
+        cy.log(`Debe poder hacer login con usuario ${user.username}`);
+        cy.login(user.username, user.password);
+        cy.url().should("include", "/inventory.html");
+        cy.go("back");
+      }
+    });
   });
 
   it('Debería no permitir hacer login con usuarios no registrados', () => {
-    const username = 'benito';
-    const password = 'miclave';
-    cy.get('input[type="text"]').should('exist').type(username);
-    cy.get('input[type="password"]').should('exist').type(password);
-    cy.get('[type="submit"]').contains('LOGIN').click();
+    dataUsers.fake_users.forEach((user) => {
+      cy.login(user.username, user.password);
+      cy.get('button[class="error-button"]').should('be.visible');
+    });
   });
   
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (user, password) => {
+      cy.get('#user-name').type(user);
+      cy.get('#password').type(password);
+      cy.contains('LOGIN').click();
+});
