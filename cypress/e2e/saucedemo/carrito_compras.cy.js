bf09c0495c963198d89468095da014f18ff860c6
German Arenas <german.arenass@autonoma.edu.co>
2024-09-02 23:03:58 -0500

    test para adicionar y remover productos del carrito

--- cypress/e2e/saucedemo/carrito_compras.cy.js
+++ cypress/e2e/saucedemo/carrito_compras.cy.js
@@ -20,24 +20,82 @@ describe("Carrito de compras", () => {
     const cantElementosCarrito = 6;
     loginUserStandard();
     cy.url().should("eq", `${Cypress.config("baseUrl")}/inventory.html`);
+    add_products();
+
+    cy.get(".fa-layers-counter.shopping_cart_badge").should(
+      "have.text",
+      cantElementosCarrito
+    );
+  });
+
+  it("Eliminar 4 elementos del carrito de compras", () => {
+    loginUserStandard();
+    cy.url().should("eq", `${Cypress.config("baseUrl")}/inventory.html`);
+    add_products();
+    remove_products(4);
+  });
+
+  function add_products() {
     cy.get(".inventory_item").each(($el) => {
       cy.wrap($el)
         .find(".inventory_item_name")
         .invoke("text")
         .then((text) => {
           const itemName = text.trim();
-          expect(itemName).to.equal(
-            findProductName(data_products.products, itemName)
+          const productAdd = findProductName(
+            data_products.add_products,
+            itemName
           );
+          if (productAdd) {
+            cy.wrap($el)
+              .find(".btn_primary.btn_inventory")
+              .should("exist")
+              .and("be.visible")
+              .contains("ADD TO CART")
+              .click();
+          }
         });
-
-      cy.wrap($el).find(".btn_primary.btn_inventory").should("exist").click();
     });
-    cy.get(".fa-layers-counter.shopping_cart_badge").should(
-      "have.text",
-      cantElementosCarrito
-    );
-  });
+  }
+
+  function remove_products(amountRemove) {
+    let cantRemove = 0;
+
+    cy.get(".inventory_item")
+      .each(($el) => {
+        cy.wrap($el)
+          .find(".inventory_item_name")
+          .should("exist")
+          .invoke("text")
+          .then((text) => {
+            const textName = text.trim();
+            const productRemove = findProductName(
+              data_products.remove_products,
+              textName
+            );
+
+            if (productRemove) {
+              cy.wrap($el)
+                .find(".btn_secondary.btn_inventory")
+                .should("exist")
+                .and("be.visible")
+                .contains("REMOVE")
+                .click()
+                .then(() => {
+                  cantRemove++;
+                });
+            }
+          });
+      })
+      .then(() => {
+        expect(cantRemove).to.equal(amountRemove);
+      });
+  }
+
+  function findProductName(data, productName) {
+    const product = data.find((prod) => prod.name === productName);
+    if (product) return product.name;
+  }
 
   function loginUserStandard() {
     const user = data_users.users.find(
@@ -47,10 +105,5 @@ describe("Carrito de compras", () => {
     if (user) {
       cy.login(user.username, user.password);
     }
-  }
-
-  function findProductName(data, productName) {
-    const product = data.find((prod) => prod.name === productName);
-    if (product) return product.name;
   }
 });
